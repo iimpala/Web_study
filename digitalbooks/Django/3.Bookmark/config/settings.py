8d4d17feb6b5243e3a0a09fe945ef418259bcff9
iimpala <dla4051@naver.com>
2022-08-29 18:08:44 +0900

    add static root

--- digitalbooks/Django/3.Bookmark/config/settings.py
+++ digitalbooks/Django/3.Bookmark/config/settings.py
@@ -122,6 +122,8 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [path.join(BASE_DIR, 'static'),]
 
+STATIC_ROOT = path.join(BASE_DIR, 'static_files')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
